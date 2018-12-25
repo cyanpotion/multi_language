2cc75101028e029c558cb0c4d8b4d8a617fae058
XenoAmess <xenoamess@gmail.com>
2018-12-26 05:42:26 +0800

    0.38
    add ifLanguageNeedWordWrap

--- src/main/java/com/xenoamess/multi_language/MultiLanguageStructure.java
+++ src/main/java/com/xenoamess/multi_language/MultiLanguageStructure.java
@@ -9,6 +9,37 @@ import java.util.Map;
  * and use MultiLanguageX8lFileUtil.parse to generate this MultiLanguageStructure object.
  */
 public class MultiLanguageStructure {
+    public static final String arabic = "arabic";
+    public static final String bulgarian = "bulgarian";
+    public static final String schinese = "schinese";
+    public static final String tchinese = "tchinese";
+    public static final String czech = "czech";
+    public static final String danish = "danish";
+    public static final String dutch = "dutch";
+    public static final String english = "english";
+    public static final String finnish = "finnish";
+    public static final String french = "french";
+    public static final String german = "german";
+    public static final String greek = "greek";
+    public static final String hungarian = "hungarian";
+    public static final String italian = "italian";
+    public static final String japanese = "japanese";
+    public static final String koreana = "koreana";
+    public static final String norwegian = "norwegian";
+    public static final String polish = "polish";
+    public static final String portuguese = "portuguese";
+    public static final String brazilian = "brazilian";
+    public static final String romanian = "romanian";
+    public static final String russian = "russian";
+    public static final String spanish = "spanish";
+    public static final String latam = "latam";
+    public static final String swedish = "swedish";
+    public static final String thai = "thai";
+    public static final String turkish = "turkish";
+    public static final String ukrainian = "ukrainian";
+    public static final String vietnamese = "vietnamese";
+
+
     public Map<String, SingleLanguageStructure> languageMap = new HashMap<String, SingleLanguageStructure>();
     public String currentLanguage;
 
@@ -67,6 +98,21 @@ public class MultiLanguageStructure {
         for (Map.Entry<String, SingleLanguageStructure> entry : languageMap.entrySet()) {
             System.out.println(entry.getKey());
             System.out.println(entry.getValue());
+        }
+    }
+
+    public boolean ifLanguageNeedWordWrap(String languageName) {
+        switch (languageName) {
+            case schinese:
+                return false;
+            case tchinese:
+                return false;
+            case japanese:
+                return false;
+            case koreana:
+                return false;
+            default:
+                return true;
         }
     }
 
